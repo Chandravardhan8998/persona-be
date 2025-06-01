ddd7756965a130ae119c264a0cb65f173c524fae
DX-ChandraVS <chandravardhansingh1@gmail.com>
2025-06-02 03:12:55 +0530

    few fix

--- redis_config.py
+++ redis_config.py
@@ -1,19 +1,17 @@
-import redis
+import redis.asyncio as redis
 from dotenv import load_dotenv
 import os
-import ssl
-import certifi
 
 load_dotenv()
 
 redis_url = os.getenv("REDIS_HOST")
 redis_pass = os.getenv("REDIS_PASS")
-
 r = redis.Redis(
   host=redis_url,
   port=6379,
   password=redis_pass,
   ssl=True
+
 )
 try:
     pong = r.ping()
