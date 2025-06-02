0f9dedc5f43e5b1c390c9017af3cabd55ccc7a58
DX-ChandraVS <chandravardhansingh1@gmail.com>
2025-06-02 17:09:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ r = redis.Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
